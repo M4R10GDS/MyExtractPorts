8274a95daca1e332eedb1d58039d05c1e51b1012
MRM4R10 <magodesauni@gmail.com>
2022-07-12 11:17:09 +0200

    global tool paths added

--- extractPorts.sh
+++ extractPorts.sh
@@ -16,7 +16,7 @@ WHT='\033[0;37m'        # White
 
 #Exit function
 ctrl_c(){
-	echo -e "\n\n${RED}[+]${CLF} Exiting...\n"
+	/usr/bin/echo -e "\n\n${RED}[+]${CLF} Exiting...\n"
 	exit 1
 }
 
@@ -27,8 +27,8 @@ trap ctrl_c INT
 
 #Help msg
 usage(){
-	echo -e "${CYAN}[+]${CLF} Help mode. Usage example:\n"
-	echo -e "\textractPorts <source_file_name> <dest_file_name (OPTIONAL)>\n"
+	/usr/bin/echo -e "${CYAN}[+]${CLF} Help mode. Usage example:\n"
+	/usr/bin/echo -e "\textractPorts <source_file_name> <dest_file_name (OPTIONAL)>\n"
 	return 0
 }
 
@@ -36,39 +36,39 @@ usage(){
 #Extracts all ports
 get_ports(){
 	#Useless data + ports info
-	all_data=$(grep "Ports:" $1)
+	all_data=$(/usr/bin/grep "Ports:" $1)
 
 	#Just ports info
-	ports_data=$(echo $all_data | awk '{ print $2 }' FS='Ports:')
+	ports_data=$(/usr/bin/echo $all_data | /usr/bin/awk '{ print $2 }' FS='Ports:')
 
 	#Number of ports
-	n_ports=$(echo $ports_data | awk -F',' '{ print NF }')
+	n_ports=$(/usr/bin/echo $ports_data | /usr/bin/awk -F',' '{ print NF }')
 
 	#Ports accumulator
-	all_acc_ports=$(echo $(echo $ports_data | awk -F',' '{ print $1 }' | awk -F'/' '{ print $1 }' | tr -d '\n'))
+	all_acc_ports=$(/usr/bin/echo $(/usr/bin/echo $ports_data | /usr/bin/awk -F',' '{ print $1 }' | /usr/bin/awk -F'/' '{ print $1 }' | /usr/bin/tr -d '\n'))
 
 	#Next port to analyze
 	new_port=""
 
-	echo -e "\n\t${PPL}[+]${CLF} $n_ports ports founded.\n"
-	echo -e "${CYAN}[+]${CLF} Extracting data..."
+	/usr/bin/echo -e "\n\t${PPL}[+]${CLF} $n_ports ports founded.\n"
+	/usr/bin/echo -e "${CYAN}[+]${CLF} Extracting data..."
 
 	for i in $(seq 2 $n_ports);
 	do
-		new_port=$(echo $(echo $ports_data | awk -F',' '{ print $'$i' }' | awk -F'/' '{ print $1 }' | tr -d '\n'))
-		all_acc_ports=$(echo "$all_acc_ports,$new_port")
+		new_port=$(/usr/bin/echo $(/usr/bin/echo $ports_data | /usr/bin/awk -F',' '{ print $'$i' }' | /usr/bin/awk -F'/' '{ print $1 }' | /usr/bin/tr -d '\n'))
+		all_acc_ports=$(/usr/bin/echo "$all_acc_ports,$new_port")
 	done
 
 	if [ $# == 2 ]; then
 		create_report_file $2 $all_acc_ports
 	fi
 
-	echo -e "${CYAN}[+]${CLF} Copying to clipboard..."
-	(echo -n $all_acc_ports | xclip -sel clip) 2>/dev/null
-	echo -e "${PPL}[+]${CLF} Ports discovered:\n"
+	/usr/bin/echo -e "${CYAN}[+]${CLF} Copying to clipboard..."
+	(/usr/bin/echo -n $all_acc_ports | /usr/bin/xclip -sel clip) 2>/dev/null
+	/usr/bin/echo -e "${PPL}[+]${CLF} Ports discovered:\n"
 
-	echo -e "\t$all_acc_ports"
-	echo
+	/usr/bin/echo -e "\t$all_acc_ports"
+	/usr/bin/echo
 	return 0
 }
 
@@ -76,23 +76,23 @@ get_ports(){
 #Saves the output in a file
 create_report_file(){
 	if [ -f $1 ]; then
-		echo -n -e "\n${YLW}[WARNING]${CLF} This file already exists, do you want to overwrite it?[y/n]: "
+		/usr/bin/echo -n -e "\n${YLW}[WARNING]${CLF} This file already exists, do you want to overwrite it?[y/n]: "
 		read usr_input
 
 		if [ $usr_input == "n" ]; then
 			return 1
 		fi
 	fi
-	echo -e "${CYAN}[+]${CLF} Saving data..."
-	(echo -e "${PPL}[+]${CLF} Ports discovered:\n" > $1) 2> /dev/null
+	/usr/bin/echo -e "${CYAN}[+]${CLF} Saving data..."
+	(/usr/bin/echo -e "${PPL}[+]${CLF} Ports discovered:\n" > $1) 2> /dev/null
 
 	if [ $? == 1 ]; then
-		echo -e "${RED}[ERROR]${CLF} Error writing to file. Make sure you have write permissions."
+		/usr/bin/echo -e "${RED}[ERROR]${CLF} Error writing to file. Make sure you have write permissions."
 		return 1
 	fi
-	echo $2 >> $1
+	/usr/bin/echo $2 >> $1
 
-	echo -e "${PPL}[+]${CLF} Data saved."
+	/usr/bin/echo -e "${PPL}[+]${CLF} Data saved."
 	return 0
 }
 
@@ -100,14 +100,14 @@ create_report_file(){
 if [ $# == 0 ] || [ $# -gt 2 ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
 	usage
 else
-	echo -e "${CYAN}[+]${CLF} Reading file..."
+	/usr/bin/echo -e "${CYAN}[+]${CLF} Reading file..."
 	if [ -f  $1 ]; then
 
 		#Check if its an nmap -oG file
-		echo -e "${CYAN}[+]${CLF} Checking nmap headers..."
+		/usr/bin/echo -e "${CYAN}[+]${CLF} Checking nmap headers..."
 
-		first_line=$(head -n 1 $1 | awk '{ print $2 };')
-		last_line=$(tail -n 1 $1 | awk '{ print $2 };')
+		first_line=$(/usr/bin/head -n 1 $1 | /usr/bin/awk '{ print $2 };')
+		last_line=$(/usr/bin/tail -n 1 $1 | /usr/bin/awk '{ print $2 };')
 
 		#Strings length
 		f_s=${#first_line}
@@ -115,16 +115,16 @@ else
 
 		#Check if the file has Nmap structure looking for key words
 		if [ $f_s -gt 0 ] && [ $l_s -gt 0 ] && [ $first_line == $last_line ] && [ $first_line == "Nmap" ]; then
-			echo -e "${PPL}[+]${CLF} Nmap file founded:"
+			/usr/bin/echo -e "${PPL}[+]${CLF} Nmap file founded:"
 
 			get_ports $1 $2
 
 		else
-			echo -e "${RED}[ERROR]${CLF} Error validating nmap structure. Make sure to use the -oG Nmap option and try again.\n"
+			/usr/bin/echo -e "${RED}[ERROR]${CLF} Error validating nmap structure. Make sure to use the -oG Nmap option and try again.\n"
 			exit 1
 		fi
 	else
-		echo -e "${RED}[ERROR]${CLF} File not found.\n"
+		/usr/bin/echo -e "${RED}[ERROR]${CLF} File not found.\n"
 		exit 1
 	fi
 fi
